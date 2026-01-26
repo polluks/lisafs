8f36d5d846d680c231e204dfb4175c3722d556f5
Chris Hanson <cmh@basehit.ventures>
2026-01-25 18:39:29 -0800

    Cache s-files, expand MF tag to label.

--- src/lisafs.c
+++ src/lisafs.c
@@ -27,6 +27,9 @@ struct lisafs_image {
     // Cached from MDDF
     lisafs_paddr slist_addr;
     lisafs_paddr root_page;
+
+    // Cached from the image.
+    lisafs_s_entry * _Nullable s_files;
 };
 
 
@@ -180,6 +183,51 @@ error:
 }
 
 
+int lisafs_image_cache_s_files(lisafs_image * _Nonnull image)
+{
+    lisafs_paddr s_files_start = image->mddf.slist_addr;
+    const size_t slist_block_count = image->mddf.slist_block_count;
+
+    // The s-list itself is cached rather than the blocks containing it.
+
+    const size_t slist_packing = image->mddf.slist_packing;
+    const size_t slist_max_entries = slist_packing * slist_block_count;
+    image->s_files = calloc(sizeof(lisafs_s_entry), slist_max_entries);
+    if (image->s_files == NULL) {
+        errno = ENOMEM;
+        goto error;
+    }
+
+    // Go through each block, copying and converting all the s-list
+    // entries that it contains.
+
+    for (int b = 0; b < slist_block_count; b++) {
+        lisafs_page page = {0};
+        lisafs_pagelabel label = {0};
+
+        int read_err = lisafs_image_read_page(image, s_files_start + b, page, &label);
+        if (read_err == -1) goto error;
+
+        lisafs_s_entry *slist_page_entries = (lisafs_s_entry *)page;
+        for (int i = 0; i < slist_packing; i++) {
+            const int entry_idx = slist_packing * b + i;
+            lisafs_s_entry *entry = &image->s_files[entry_idx];
+            lisafs_s_entry *raw_entry = &slist_page_entries[i];
+
+            entry->hintaddr = swap32(raw_entry->hintaddr);
+            entry->fileaddr = swap32(raw_entry->fileaddr);
+            entry->filesize = swap32(raw_entry->filesize);
+            entry->version  = swap16(raw_entry->version);
+        }
+    }
+
+    return 0;
+
+error:
+    return -1;
+}
+
+
 lisafs_image * _Nullable lisafs_image_open(const char * _Nonnull const path)
 {
     lisafs_image *image = calloc(sizeof(lisafs_image), 1);
@@ -206,6 +254,11 @@ lisafs_image * _Nullable lisafs_image_open(const char * _Nonnull const path)
     int mddf_err = lisafs_image_read_mddf(image);
     if (mddf_err) goto error;
 
+    // Read and validate the S-files.
+
+    int s_files_err = lisafs_image_cache_s_files(image);
+    if (s_files_err) goto error;
+
     return image;
 
 error:
@@ -218,6 +271,9 @@ int lisafs_image_close(lisafs_image * _Nullable image)
     if (image == NULL) return 0;
 
     int savederrno = errno; // don't let the act of saving destroy errno
+
+    free(image->s_files);
+    image->s_files = NULL;
 
     image_dc42_close(image->image);
     image->image = NULL;
@@ -273,19 +329,66 @@ int lisafs_image_read_page(lisafs_image * _Nonnull image,
 {
     assert(image->image != NULL);
 
-    lisafs_baddr real_n = image->block0 + n;
+    const lisafs_baddr real_n = image->block0 + n;
 
-    lisafs_tag tag;
+    uint8_t tag[12];
     int read_err = image_dc42_read_block(image->image, real_n, page, tag);
     if (read_err == -1) return -1;
 
     // Expand tag into label.
 
-    // TODO: Expand tag into label.
+    // Since the Sony microfloppy doesn't have enough space for the full
+    // 24-byte page label, that information is compressed cleverly in a
+    // way that takes advantage of the fact that it will never need
+    // 32-bit relative page addresses and that the absolute page address
+    // doesn't need to be stored. The exact algorithm is derivable from
+    // the FINISH_READ function in the -OS-SOURCE/SONYASM.TEXT source
+    // file.
 
-    // The tag doens't contain the absolute page, instead it expects the
-    // driver to set that based on the block being read.
-    label->abspage = real_n;
+    // The first few values are encoded directly.
+
+    int16_t tag_version = *((int16_t *) &tag[0]);
+    int16_t tag_flags   = *((int16_t *) &tag[2]);
+    int16_t tag_fileid  = *((int16_t *) &tag[4]);
+    int16_t tag_relpage = *((int16_t *) &tag[6]);
+    int16_t tag_fwdldu  = *((int16_t *) &tag[8]);
+    int16_t tag_bkwdldu = *((int16_t *) &tag[10]);
+
+    label->version   = swap16(tag_version);
+    label->flags     = swap16(tag_flags);
+    label->fileid    = swap16(tag_fileid);
+    label->abspage   = real_n;
+    label->relpage   = swap16(tag_relpage); // 16-to-32 expansion here
+
+    // The data used and backwards/forwards link fields are encoded
+    // cleverly, since a microfloppy is of limited size; this means a
+    // file can only be so large, and the relative links can only reach
+    // so far. Thus the data used value is kept in the high bits of the
+    // link fields, while the link values are kept in the low bits of
+    // the links, and if the link value is 0x07ff it's expanded to
+    // 0xffffffff.
+
+    int16_t fwdldu   = swap16(tag_fwdldu);
+    int16_t bkwdldu  = swap16(tag_bkwdldu);
+    int16_t dataused = (((bkwdldu & 0xf800) >> 5) + (fwdldu & 0xf800)) >> 6;
+
+#define LISAFS_EXTLINK(l) (((l & 0x07ff) != 0x7ff) ? (l & 0x07ff) : 0xffffffff)
+    label->dataused  = dataused;
+    label->fwdlink   = LISAFS_EXTLINK(fwdldu);
+    label->bkwdlink  = LISAFS_EXTLINK(bkwdldu);
+#undef LISAFS_EXTLINK
 
     return 0;
 }
+
+
+// MARK: - Directory B-Tree Elements
+
+struct lisafs_key {
+	lisafs_byte key_length;
+	lisafs_integer parent_id;
+	char name[33];
+	char name_pad;
+} LISAFS_PACKED;
+typedef struct lisafs_key lisafs_key;
+
